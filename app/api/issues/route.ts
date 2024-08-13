709fcc494530d0d6262bd996cd6787def8665e74
Damilola Osunsanwo <82976159+ddddami@users.noreply.github.com>
2024-08-13 16:49:41 +0100

    Handle errors in issues form

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -5,8 +5,8 @@ import prisma from "@/prisma/client";
 const STATUSES = ["OPEN", "IN_PROGRESS", "CLOSED"] as const;
 
 const createIssueSchema = z.object({
-  title: z.string().min(3).max(255),
-  description: z.string().min(3).max(2048),
+  title: z.string().min(3, "Title is required.").max(255),
+  description: z.string().min(3, "Description is required."),
   status: z.enum(STATUSES).optional(),
 });
 
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,27 +1,38 @@
 "use client";
 
-import { Button, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextField } from "@radix-ui/themes";
+import axios from "axios";
+import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { Controller, useForm } from "react-hook-form";
 import SimpleMdeReact from "react-simplemde-editor";
-import "easymde/dist/easymde.min.css";
-import axios from "axios";
-import { useRouter } from "next/navigation";
+interface IssueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
-  interface IssueForm {
-    title: string;
-    description: string;
-  }
+  const [error, setError] = useState("");
   const router = useRouter();
   const { register, control, handleSubmit } = useForm<IssueForm>();
   return (
     <form
       onSubmit={handleSubmit(async (data) => {
-        await axios.post("/api/issues", {...data, });
-        router.push("/issues");
+        try {
+          await axios.post("/api/issues", { ...data });
+          router.push("/issues");
+        } catch (error) {
+          setError("An unexpected error occured.");
+        }
       })}
       className="max-w-xl space-y-3"
     >
+      {error && (
+        <Callout.Root color="red">
+          <Callout.Text>{error}</Callout.Text>
+        </Callout.Root>
+      )}
       <TextField.Root
         placeholder="Title"
         {...register("title")}
