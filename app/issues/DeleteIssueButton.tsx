bd22cad2192c89744f1da90caa7173c448d16787
Damilola Osunsanwo <82976159+ddddami@users.noreply.github.com>
2024-08-29 20:27:35 +0100

    Add a confirmation dialog box

--- app/issues/DeleteIssueButton.tsx
+++ app/issues/DeleteIssueButton.tsx
@@ -1,13 +1,33 @@
-import Link from "next/link";
-import { Button } from "@radix-ui/themes";
+import { AlertDialog, Button, Flex } from "@radix-ui/themes";
 import { Trash2 } from "lucide-react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   return (
-    <Button color="red">
-      <Trash2 size={20} strokeWidth={1.25} />
-      <Link href={`/issues/${issueId}/delete`}>Delete Issue</Link>
-    </Button>
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">
+          <Trash2 size={20} strokeWidth={1.25} />
+          Delete Issue
+        </Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Delete Issue</AlertDialog.Title>
+        <AlertDialog.Description>
+          Are you sure you want to delete this issue? this action cannot be
+          undone.
+        </AlertDialog.Description>
+        <Flex gap="3" className="mt-3">
+          <AlertDialog.Cancel>
+            <Button color="gray" variant="soft">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button color="red">Delete</Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
   );
 };
 
