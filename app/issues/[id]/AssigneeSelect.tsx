39baa3b82a06ba6605c64f8355727c7d7ac7575c
Damilola Osunsanwo <82976159+ddddami@users.noreply.github.com>
2024-09-10 10:06:45 +0100

    Refactor- AssigneeSelect

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -2,17 +2,20 @@
 
 import { Select, Skeleton } from "@radix-ui/themes";
 import axios from "axios";
-import { Issue, User } from "@prisma/client";
 import { useQuery } from "@tanstack/react-query";
 import toast, { Toaster } from "react-hot-toast";
+import { Issue, User } from "@prisma/client";
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
-  const { data: users, isLoading } = useQuery({
-    queryKey: ["users"],
-    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
-    staleTime: 60 * 1000,
-    retry: 3,
-  });
+  const { data: users, isLoading } = useUsers();
+
+  const assignIssue = (userId: string) => {
+    axios
+      .patch("/api/issues/" + issue.id, {
+        assignedUserId: userId === "unassigned" ? null : userId,
+      })
+      .catch(() => toast.error("Changes could not be saved."));
+  };
 
   if (isLoading) return <Skeleton />;
   return (
@@ -20,13 +23,7 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
       <Toaster />
       <Select.Root
         defaultValue={issue.assignedUserId || "unassigned"}
-        onValueChange={(userId) => {
-          axios
-            .patch("/api/issues/" + issue.id, {
-              assignedUserId: userId === "unassigned" ? null : userId,
-            })
-            .catch(() => toast.error("Changes could not be saved."));
-        }}
+        onValueChange={assignIssue}
       >
         <Select.Trigger placeholder="Assign..." />
         <Select.Content>
@@ -44,5 +41,13 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
     </>
   );
 };
+
+const useUsers = () =>
+  useQuery({
+    queryKey: ["users"],
+    queryFn: () => axios.get<User[]>("/api/users").then((res) => res.data),
+    staleTime: 60 * 1000 * 10,
+    retry: 3,
+  });
 
 export default AssigneeSelect;
