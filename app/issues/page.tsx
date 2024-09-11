2e495d09353da37666801433deef146a138d7c07
Damilola Osunsanwo <82976159+ddddami@users.noreply.github.com>
2024-09-11 09:52:11 +0100

    Sort issues

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -11,19 +11,27 @@ const IssuesPage = async ({
 }: {
   searchParams: { status: Status; orderBy: keyof Issue };
 }) => {
-  const statuses = Object.values(Status);
-  const status = statuses.includes(searchParams.status)
-    ? searchParams.status
-    : undefined;
-  const issues = await prisma.issue.findMany({
-    where: { status },
-  });
-
   const columns: { label: string; value: keyof Issue; className?: string }[] = [
     { label: "Title", value: "title" },
     { label: "Status", value: "status", className: "hidden md:table-cell" },
     { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
   ];
+
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
+    
+  const issues = await prisma.issue.findMany({
+    where: { status },
+    orderBy,
+  });
 
   return (
     <div>
